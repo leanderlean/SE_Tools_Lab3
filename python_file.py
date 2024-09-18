28c6dc4c912cd8732397e2a2ee0041b680ec44fe
Lean <leandergalido328@gmail.com>
2024-09-18 15:06:23 +0800

    Added features that sorts the name alphabetically

--- python_file.py
+++ python_file.py
@@ -1,9 +1,14 @@
 #enrollment system
 
 student_info_list = []
-student_name = input("Please input student name: ")
-student_age = input("Please input age: ")
-student_grade_level = input("What grade level would you like to enroll? ")
-student_info_list.append((f"{student_name}, age: {student_age}, enrolled in grade: {student_grade_level}"))
-print(f"{student_name} is successfully enrolled")
-print(student_info_list)
+
+while True:
+    student_name = input("Please input student name (or type 'exit' to stop): ")
+    if student_name.lower() == 'exit':
+        break
+    student_age = input("Please input age: ")
+    student_grade_level = input("What grade level would you like to enroll? ")
+    student_info_list.append((f"{student_name}, age: {student_age}, enrolled in grade: {student_grade_level}"))
+    print(f"{student_name} is successfully enrolled")
+
+print(sorted(student_info_list))
