76a708aae7e51a54e81125913599686f11db9454
Lean <164350384+leanderlean@users.noreply.github.com>
2024-09-18 16:49:01 +0800

    fix all the conflicts

--- python_file.py
+++ python_file.py
@@ -39,15 +39,3 @@ while True:
     print(f"{student_name} is successfully enrolled")
 
 print(sorted(student_info_list))
-
-
-while True:
-    student_name = input("Please input student name (or type 'exit' to stop): ")
-    if student_name.lower() == 'exit':
-        break
-    student_age = input("Please input age: ")
-    student_grade_level = input("What grade level would you like to enroll? ")
-    student_info_list.append((f"{student_name}, age: {student_age}, enrolled in grade: {student_grade_level}"))
-    print(f"{student_name} is successfully enrolled")
-
-print(sorted(student_info_list))
